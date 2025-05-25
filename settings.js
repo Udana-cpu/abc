ac80d5cae43c58e7fb76be9091bb21f4be93a4f7
Udana-cpu <udana.suwahas.2007@gmail.com>
2025-05-25 20:45:27 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -4,7 +4,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-
+    
 SESSION_ID: process.env.SESSION_ID === undefined ? '𝙰𝚂𝙸𝚃𝙷𝙰-𝙼𝙳=EVplSK7D#I1gvEnA_KgSggQTOnL06JzleYj1kmPjaf-4qG9uKA3U' : process.env.SESSION_ID,
 PORT: process.env.PORT === undefined ? "8000" : process.env.PORT,
 SESSION_NAME: process.env.PORT === undefined ? "asitha" : process.env.SESSION_NAME,
